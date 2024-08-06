ac5b122b952f531d28968ad399a82edfdca61f27
Stephen Ochieng <materiamystic@gmail.com>
2024-08-06 17:27:25 +0300

    Adds sqlite for testing purposes

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ app = Flask(__name__,
     static_folder='../client/build',
     template_folder='../client/build')
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI') #'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
 app.json.compact = False
