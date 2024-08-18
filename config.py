e7aba44568a30e09076dcfece6970ed9ec95d0fc
Stephen Ochieng <materiamystic@gmail.com>
2024-08-18 13:58:51 +0300

    configures DB_URI

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 # Standard library imports
-from dotenv import load_dotenv
+
 import os
+from dotenv import load_dotenv
 # Remote library imports
 from flask import Flask
 from flask_cors import CORS
@@ -34,7 +35,7 @@ mail = Mail(app)
 
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres.khqalbapxdbyuuscsdww:w!hSBVH!VF96SR@aws-0-eu-central-1.pooler.supabase.com:6543/postgres'#os.getenv('DATABASE_URI') #'sqlite:///app.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')#os.getenv('DATABASE_URI') #'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
 app.json.compact = False
